36ec558888da1c31d42d197c4eacdba95e3a2f60
ShaymaAbuZahra <shayma.abuzahra@outlook.com>
2016-08-15 22:36:07 +0300

    Initial Commit

---
+++ testForGitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testForGitHub
+//
+//  Created by Shayma Abu Zahra on 8/15/16.
+//  Copyright © 2016 Shayma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
